dceb61c86655c4c2887476857480e8633e98e883
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-03-21 16:41:26 +0200

    +searchParams

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
+
+const inputRef = document.querySelector('#search-form');
+const submitRef = document.querySelector('.btn-search');
+
+
+
+//1. отримати доступ до  Pixabay. через axios
+inputRef.addEventListener('submit', onSubimitClick);
+
+function onSubimitClick(e) {
+    e.preventDefault();
+    const searchQuery = e.currentTarget.elements.searchQuery.value;
+
+// console.log(e.currentTarget.elements.searchQuery.value);
+    const searchParams = new URLSearchParams({
+        q: searchQuery,
+        image_type:"photo",
+        oriental:"horizontal",
+        per_page: 40,
+        safesearch:true,
+    })
+  
+    axios.get(`https://pixabay.com/api/?key=34599665-397fd73b742fe3f801288186c&${searchParams}`).then(response => console.log(response))
+    .catch((error) => { console.error(error) });
+}
